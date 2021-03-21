7fe50bf01ef25a6cf679c965af7a5e9ccf22663b
Arnut Khummin <arnut.khu@gmail.com>
2021-03-21 22:36:40 +0700

    styled login

--- pages/login.vue
+++ pages/login.vue
@@ -1,42 +1,109 @@
 <template>
-  <div class="container min-w-lg">
-    <h3>Welcome</h3>
-    <section>
-      <form @submit="login">
-        <label for="username">ชื่อผู้ใช้</label>
-        <input type="text" name="username" placeholder="กรุณาเข้าสู่ระบบ" v-model="username"/>
+  <div class="body-bg min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0">
+    <header class="max-w-lg mx-auto">
+      <a href="#">
+        <h1 class="text-4xl font-bold text-white text-center">Startup</h1>
+      </a>
+    </header>
 
-        <label for="password">รหัสผ่าน</label>
-        <input type="password" name="password" placeholder="***********" v-model="password"/>
-        <input type="submit" value="เข้าสู่ระบบ" />
-      </form>
-    </section>
+    <main
+      class="bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl"
+    >
+      <section>
+        <h3 class="font-bold text-2xl">Welcome to Startup</h3>
+        <p class="text-gray-600 pt-2">Sign in to your account.</p>
+      </section>
+
+      <section class="mt-10">
+        <form class="flex flex-col" @submit="login">
+          <div class="mb-6 pt-3 rounded bg-gray-200">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2 ml-3"
+              for="email"
+              >Email</label
+            >
+            <input
+              type="text"
+              id="email"
+              v-model="username"
+              class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
+            />
+          </div>
+          <div class="mb-6 pt-3 rounded bg-gray-200">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2 ml-3"
+              for="password"
+              >Password</label
+            >
+            <input
+              type="password"
+              id="password"
+              v-model="password"
+              class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
+            />
+          </div>
+          <div class="flex justify-end">
+            <a
+              href="#"
+              class="text-sm text-purple-600 hover:text-purple-700 hover:underline mb-6"
+              >Forgot your password?</a
+            >
+          </div>
+          <button
+            class="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200"
+            type="submit"
+          >
+            Sign In
+          </button>
+        </form>
+      </section>
+    </main>
+
+    <div class="max-w-lg mx-auto text-center mt-12 mb-6">
+      <p class="text-white">
+        Don't have an account?
+        <a href="#" class="font-bold hover:underline">Sign up</a>.
+      </p>
+    </div>
+
+    <footer class="max-w-lg mx-auto flex justify-center text-white">
+      <a href="#" class="hover:underline">Contact</a>
+      <span class="mx-3">•</span>
+      <a href="#" class="hover:underline">Privacy</a>
+    </footer>
   </div>
 </template>
 
 <script>
 import {} from '@nuxtjs/axios'
 export default {
-    data() {
+  data() {
     return {
       username: '',
-      password: ''
-    };
+      password: '',
+    }
   },
   methods: {
     async login(e) {
-      e.preventDefault();
+      e.preventDefault()
       const payload = {
         username: this.username,
-        password: this.password
-      };
+        password: this.password,
+      }
       try {
         await this.$auth.loginWith('local', { data: payload })
         this.$router.push('/')
       } catch (e) {
         this.$router.push('/login')
       }
-    }
-  }
+    },
+  },
 }
 </script>
+
+<style scoped>
+.body-bg {
+  background-color: #9921e8;
+  background-image: linear-gradient(315deg, #9921e8 0%, #5f72be 74%);
+}
+</style>
