37f09b65dd169c71efaf5a4310a3f70266006164
rnut <rnut.ist@gmail.com>
2021-04-02 21:53:38 +0700

    add new page

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,10 @@
 <template>
   <div class="w-full h-full p-16 overflow-auto">
-    <section class="flex flex-row flex-wrap items-center">
-      <button
-        v-for="item in items"
-        :key="item.id"
-        class="transition duration-600 ease-in transform hover:scale-125 rounded-md bg-white border border-gray-100 py-8 px-8 m-4 shadow"
-      >
-        <p
-          class="text-2xl text-left tracking-wide overflow-ellipsis overflow-hidden text-gray-700"
-        >
-          หุ้นไต้หวัน
-        </p>
-        <p
-          class="text-2xl text-left tracking-wide overflow-ellipsis overflow-hidden text-gray-500"
-        >
-          รอบ(3) 05:00
-        </p>
-        <div class="rounded-full bg-pink-100 mt-4 pl-4 pr-2 py-1">
-          <span class="text-sm text-pink-900">เหลือเวลา 56 นาที</span>
-        </div>
-      </button>
-    </section>
+    <button class="bg-red-300">
+      <!-- <NuxtLink to="/member/board">
+        About (internal link that belongs to the Nuxt App)
+      </NuxtLink> -->
+    </button>
   </div>
 </template>
 
@@ -70,8 +54,8 @@ export default {
   },
   methods: {
     async logout() {
-      await this.$auth.logout()
-      this.$router.push('/login')
+      // await this.$auth.logout()
+      // this.$router.push('/login')
     }
   }
   // middleware: 'auth',
--- pages/member/market.vue
+++ pages/member/market.vue
@@ -1,5 +1,77 @@
 <template>
-  <div class="p-8">
+  <div class="w-full h-full p-16 overflow-auto">
     <h1 class="font-bold">Market</h1>
+    <section class="flex flex-row flex-wrap items-center">
+      <button
+        v-for="item in items"
+        :key="item.id"
+        class="transition duration-600 ease-in transform hover:scale-125 rounded-md bg-white border border-gray-100 py-8 px-8 m-4 shadow"
+      >
+        <p
+          class="text-2xl text-left tracking-wide overflow-ellipsis overflow-hidden text-gray-700"
+        >
+          หุ้นไต้หวัน
+        </p>
+        <p
+          class="text-2xl text-left tracking-wide overflow-ellipsis overflow-hidden text-gray-500"
+        >
+          รอบ(3) 05:00
+        </p>
+        <div class="rounded-full bg-pink-100 mt-4 pl-4 pr-2 py-1">
+          <span class="text-sm text-pink-900">เหลือเวลา 56 นาที</span>
+        </div>
+      </button>
+    </section>
   </div>
 </template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      items: [
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {}
+      ],
+      user: this.$auth.user,
+      loggedIn: this.$auth.loggedIn
+    }
+  },
+  methods: {
+    async logout() {
+      await this.$auth.logout()
+      this.$router.push('/login')
+    }
+  }
+  // middleware: 'auth',
+}
+</script>
