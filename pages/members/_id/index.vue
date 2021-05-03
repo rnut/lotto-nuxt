e68880e87fe0369b6a3617e546c98e137c8f76b4
rnut <rnut.ist@gmail.com>
2021-05-03 15:15:28 +0700

    update users manage

--- pages/members/_id/index.vue
+++ pages/members/_id/index.vue
@@ -1,17 +1,225 @@
 <template>
-  <section>
-    <h1>{{ this.slug }}</h1>
-    <pre>
-    {{ member }}
-  </pre
-    >
+  <main class="max-w-lg mx-auto my-10">
+    <section class="">
+      <div class="font-bold text-xl flex">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24px"
+          viewBox="0 0 24 24"
+          width="24px"
+          fill="rgba(91, 33, 182, 1)"
+        >
+          <path d="M0 0h24v24H0V0z" fill="none" />
+          <path
+            d="M19.43 12.98c.04-.32.07-.64.07-.98 0-.34-.03-.66-.07-.98l2.11-1.65c.19-.15.24-.42.12-.64l-2-3.46c-.09-.16-.26-.25-.44-.25-.06 0-.12.01-.17.03l-2.49 1c-.52-.4-1.08-.73-1.69-.98l-.38-2.65C14.46 2.18 14.25 2 14 2h-4c-.25 0-.46.18-.49.42l-.38 2.65c-.61.25-1.17.59-1.69.98l-2.49-1c-.06-.02-.12-.03-.18-.03-.17 0-.34.09-.43.25l-2 3.46c-.13.22-.07.49.12.64l2.11 1.65c-.04.32-.07.65-.07.98 0 .33.03.66.07.98l-2.11 1.65c-.19.15-.24.42-.12.64l2 3.46c.09.16.26.25.44.25.06 0 .12-.01.17-.03l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.03.24.24.42.49.42h4c.25 0 .46-.18.49-.42l.38-2.65c.61-.25 1.17-.59 1.69-.98l2.49 1c.06.02.12.03.18.03.17 0 .34-.09.43-.25l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.65zm-1.98-1.71c.04.31.05.52.05.73 0 .21-.02.43-.05.73l-.14 1.13.89.7 1.08.84-.7 1.21-1.27-.51-1.04-.42-.9.68c-.43.32-.84.56-1.25.73l-1.06.43-.16 1.13-.2 1.35h-1.4l-.19-1.35-.16-1.13-1.06-.43c-.43-.18-.83-.41-1.23-.71l-.91-.7-1.06.43-1.27.51-.7-1.21 1.08-.84.89-.7-.14-1.13c-.03-.31-.05-.54-.05-.74s.02-.43.05-.73l.14-1.13-.89-.7-1.08-.84.7-1.21 1.27.51 1.04.42.9-.68c.43-.32.84-.56 1.25-.73l1.06-.43.16-1.13.2-1.35h1.39l.19 1.35.16 1.13 1.06.43c.43.18.83.41 1.23.71l.91.7 1.06-.43 1.27-.51.7 1.21-1.07.85-.89.7.14 1.13zM12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 6c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z"
+          />
+        </svg>
+        <span class="ml-2 font-bold text-2xl text-purple-800">
+          ตั้งค่าผู้ใช้งาน
+        </span>
+      </div>
+    </section>
 
-    <button @click="onDeleteMember(member)">ลบข้อมูล</button>
-  </section>
+    <section class="my-10 p-8 md:p-12 bg-white rounded-lg shadow-2xl">
+      <section>
+        <h1 class="font-bold text-xl text-gray-700">
+          ข้อมูลทั่วไปของผู้ใช้งาน
+        </h1>
+      </section>
+
+      <form class="" autocomplete="off" @submit="submit">
+        <section class="mt-10">
+          <div class="mb-6 pt-3 rounded bg-gray-200">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2 ml-3"
+              for="username"
+              >UserName</label
+            >
+            <input
+              type="text"
+              id="username"
+              v-model="member.username"
+              class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
+            />
+          </div>
+          <div class="mb-6 pt-3 rounded bg-gray-200">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2 ml-3"
+              for="name"
+              >ชื่อ</label
+            >
+            <input
+              type="text"
+              id="name"
+              v-model="member.name"
+              class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
+            />
+          </div>
+
+          <div class="mb-6 pt-3 rounded bg-gray-200">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2 ml-3"
+              for="lineID"
+              >Line</label
+            >
+            <input
+              type="text"
+              id="lineID"
+              v-model="member.lineID"
+              class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
+            />
+          </div>
+
+          <div class="mb-6 pt-3 rounded bg-gray-200">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2 ml-3"
+              for="mobileNumber"
+              >เบอร์โทรศัพท์</label
+            >
+            <input
+              type="text"
+              id="mobileNumber"
+              v-model="member.mobileNumber"
+              class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
+            />
+          </div>
+
+          <div class="mb-6 pt-3 rounded bg-gray-200">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2 ml-3"
+              for="role"
+              >สิทธิ์การใช้งาน</label
+            >
+            <input
+              disabled
+              type="text"
+              id="mobileNumber"
+              v-model="role"
+              class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
+            />
+          </div>
+
+          <div
+            class="mb-6 p-4 rounded bg-gray-200 flex items-center justify-between"
+          >
+            <p>เปิดการใช้่งาน</p>
+            <label class="switch">
+              <input
+                type="checkbox"
+                v-model="member.isActive"
+                :checked="member.isActive"
+              />
+              <div class="slider round">
+                <span class="on">ปกติ</span>
+                <span class="off">แบน</span>
+              </div>
+            </label>
+          </div>
+
+          <div
+            class="mb-6 p-4 rounded bg-gray-200 flex items-center justify-between"
+          >
+            <p>แจ้งชำระเงิน</p>
+            <label class="switch">
+              <input
+                type="checkbox"
+                :checked="member.cans.includes(confirmPaymentKey)"
+                v-on:change="onChangeConfirmPayment(member)"
+              />
+              <div class="slider round">
+                <span class="on">ได้</span>
+                <span class="off">ไม่ได้</span>
+              </div>
+            </label>
+          </div>
+        </section>
+
+        <hr class="my-8" />
+
+        <section>
+          <h3 class="font-bold text-xl">ตั้งค่าข้อมูลรางวัล</h3>
+          <p class="text-gray-600 pt-2">ส่วนลด ยอดจ่าย</p>
+        </section>
+
+        <section class="mt-10">
+          <div
+            v-for="lotto in member.lottos"
+            :key="lotto._id"
+            class="mb-6 pt-3 rounded bg-gray-200 flex gap-4"
+          >
+            <p class="pl-8 pr-4 py-4">
+              {{ lotto.title }}
+            </p>
+            <div>
+              <label
+                class="block text-gray-700 text-sm font-bold mb-2 ml-3"
+                v-bind:for="`discount-${lotto.type}`"
+              >
+                ส่วนลด
+              </label>
+              <input
+                type="text"
+                v-bind:id="`discount-${lotto.type}`"
+                v-model="lotto.discount"
+                class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
+              />
+            </div>
+            <div>
+              <label
+                class="block text-gray-700 text-sm font-bold mb-2 ml-3"
+                v-bind:for="`reward-${lotto.type}`"
+              >
+                ยอดจ่าย
+              </label>
+              <input
+                type="text"
+                v-bind:id="`reward-${lotto.type}`"
+                v-model="lotto.reward"
+                class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
+              />
+            </div>
+          </div>
+        </section>
+
+        <section class="flex flex-col">
+          <button
+            class="bg-purple-600 hover:bg-purple-700 text-white font-bold mt-8 py-2 rounded shadow-lg hover:shadow-xl transition duration-200"
+            type="submit"
+          >
+            บันทึก
+          </button>
+          <span
+            @click="onDeleteMember(member)"
+            class="cursor-pointer self-end text-sm text-red-600 hover:text-red-700 hover:underline mb-6 mt-2"
+          >
+            ลบผู้ใช้งาน
+          </span>
+        </section>
+      </form>
+    </section>
+  </main>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      modalActive: false,
+      confirmPaymentKey: 'confirmpayment'
+    }
+  },
+  computed: {
+    role: function () {
+      switch (this.member.role) {
+        case 'member':
+          return 'ผู้เล่น'
+        case 'agent':
+          return 'เอเย่น'
+        case 'admin':
+          return 'แอดมิน'
+      }
+    }
+  },
   async asyncData({ params, $axios }) {
     const slug = params.id
     const url = `${$axios.defaults.baseURL}/users/${slug}`
@@ -19,6 +227,16 @@ export default {
     return { slug, member }
   },
   methods: {
+    async submit(e) {
+      e.preventDefault()
+      const url = `${this.$axios.defaults.baseURL}/users/${this.slug}`
+      try {
+        const resp = await this.$axios.patch(url, this.member)
+        alert(`แก้ไขข้อมูลสำเร็จ`)
+      } catch (e) {
+        alert(`แก้ไขข้อมูลไม่สำเร็จ ${e.message}`)
+      }
+    },
     async onDeleteMember(member) {
       if (confirm(`ยืนยันการลบข้อมูลผู้ใช้งาน ${member.name}`)) {
         await this.deleteMember(member._id)
@@ -31,6 +249,19 @@ export default {
         this.$router.replace({ name: 'members' })
       } catch (e) {
         alert('ลบไม่สำเร็จ')
+      }
+    },
+    onChangeConfirmPayment(m) {
+      const key = this.confirmPaymentKey
+      if (m.cans == null) {
+        m.cans = [key]
+      } else {
+        const index = m.cans.indexOf(key)
+        if (index !== -1) {
+          m.cans.splice(index, 1)
+        } else {
+          m.cans.push(key)
+        }
       }
     }
   }
--- pages/members/index.vue
+++ pages/members/index.vue
@@ -1,160 +1,187 @@
 <template>
-  <section v-if="members">
-    <h1 class="text-2xl">จัดการสมาชิก</h1>
-    <button class="w-16 h-16 bg-green-300">
-      <nuxt-link to="/members/new"> สร้างผู้ใช้งาน </nuxt-link>
-    </button>
-    <table>
-      <colgroup>
-        <col class="" />
-        <col class="" />
-        <col class="" />
-        <col class="" />
-        <col class="" />
-        <col class="" />
-        <col class="" />
-      </colgroup>
-      <thead class="v-data-table-header">
-        <tr>
-          <th
-            role="columnheader"
-            scope="col"
-            aria-label="ยูเซอร์เนม: Not sorted. Activate to sort ascending."
-            aria-sort="none"
-            class="text-start sortable"
+  <section v-if="members" class="p-8">
+    <div class="flex gap-4 items-center">
+      <h1 class="text-2xl py-4 font-bold text-purple-800">จัดการสมาชิก</h1>
+      <nuxt-link to="/members/new">
+        <button
+          class="rounded bg-green-500 flex p-2 hover:bg-green-700 hover:ring-green-800"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 0 24 24"
+            width="24px"
+            fill="#FFFFFF"
           >
-            <span>ยูเซอร์เนม</span
-            ><i
-              aria-hidden="true"
-              class="v-icon notranslate v-data-table-header__icon mdi mdi-arrow-up theme--light"
-              style="font-size: 18px"
-            ></i>
-          </th>
-          <th
-            role="columnheader"
-            scope="col"
-            aria-label="ชื่อเล่น: Not sorted. Activate to sort ascending."
-            aria-sort="none"
-            class="text-start sortable"
-          >
-            <span>ชื่อเล่น</span
-            ><i
-              aria-hidden="true"
-              class="v-icon notranslate v-data-table-header__icon mdi mdi-arrow-up theme--light"
-              style="font-size: 18px"
-            ></i>
-          </th>
-          <th
-            role="columnheader"
-            scope="col"
-            aria-label="ระดับ: Not sorted. Activate to sort ascending."
-            aria-sort="none"
-            class="text-start sortable"
-          >
-            <span>ระดับ</span
-            ><i
-              aria-hidden="true"
-              class="v-icon notranslate v-data-table-header__icon mdi mdi-arrow-up theme--light"
-              style="font-size: 18px"
-            ></i>
-          </th>
-          <th
-            role="columnheader"
-            scope="col"
-            aria-label="ผู้สร้าง: Not sorted. Activate to sort ascending."
-            aria-sort="none"
-            class="text-start sortable"
-          >
-            <span>ผู้สร้าง</span
-            ><i
-              aria-hidden="true"
-              class="v-icon notranslate v-data-table-header__icon mdi mdi-arrow-up theme--light"
-              style="font-size: 18px"
-            ></i>
-          </th>
-          <th
-            role="columnheader"
-            scope="col"
-            aria-label="แจ้งชำระเงิน: Not sorted. Activate to sort ascending."
-            aria-sort="none"
-            class="text-start sortable"
-          >
-            <span>แจ้งชำระเงิน</span
-            ><i
-              aria-hidden="true"
-              class="v-icon notranslate v-data-table-header__icon mdi mdi-arrow-up theme--light"
-              style="font-size: 18px"
-            ></i>
-          </th>
-          <th
-            role="columnheader"
-            scope="col"
-            aria-label="สถานะ: Not sorted. Activate to sort ascending."
-            aria-sort="none"
-            class="text-start sortable"
-          >
-            <span>สถานะ</span
-            ><i
-              aria-hidden="true"
-              class="v-icon notranslate v-data-table-header__icon mdi mdi-arrow-up theme--light"
-              style="font-size: 18px"
-            ></i>
-          </th>
-          <th
-            role="columnheader"
-            scope="col"
-            aria-label="Actions"
-            class="text-start"
-          >
-            <span>Actions</span>
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr class="" v-for="member in members" :key="member._id">
-          <td class="text-start">{{ member.username }}</td>
-          <td class="text-start">{{ member.name }}</td>
-          <td class="text-start">{{ member.role }}</td>
-          <td class="text-start">{{ member.createdBy.name }}</td>
-          <td class="text-start">
-            <label class="switch">
-              <input
-                type="checkbox"
-                :checked="member.cans.includes(confirmPaymentKey)"
-                v-on:change="onChangeConfirmPayment(member)"
-              />
-              <div class="slider round">
-                <span class="on">ได้</span>
-                <span class="off">ไม่ได้</span>
-              </div>
-            </label>
-          </td>
-          <td class="text-start">
-            <label class="switch">
-              <input
-                type="checkbox"
-                :checked="member.isActive"
-                v-on:change="onChangeActive(member)"
-              />
-              <div class="slider round">
-                <span class="on">ปกติ</span>
-                <span class="off">ปิด</span>
-              </div>
-            </label>
-          </td>
-          <td class="text-start">
-            <button
-              type="button"
-              class=""
-              aria-haspopup="true"
-              aria-expanded="false"
+            <path d="M0 0h24v24H0V0z" fill="none" />
+            <path
+              d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
+            />
+          </svg>
+          <span class="text-white"> สร้างผู้ใช้งาน </span>
+        </button>
+      </nuxt-link>
+    </div>
+    <div class="bg-purple-600 rounded">
+      <table
+        class="table-auto w-full"
+        cellpadding="0"
+        cellspacing="0"
+        border="0"
+      >
+        <thead class="v-data-table-header">
+          <tr>
+            <th
+              role="columnheader"
+              scope="col"
+              aria-label="ยูเซอร์เนม: Not sorted. Activate to sort ascending."
+              aria-sort="none"
+              class="text-start sortable"
             >
-              <nuxt-link :to="`/members/${member._id}`"> ดูข้อมูล </nuxt-link>
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+              <span>ยูเซอร์เนม</span
+              ><i
+                aria-hidden="true"
+                class="v-icon notranslate v-data-table-header__icon mdi mdi-arrow-up theme--light"
+                style="font-size: 18px"
+              ></i>
+            </th>
+            <th
+              role="columnheader"
+              scope="col"
+              aria-label="ชื่อเล่น: Not sorted. Activate to sort ascending."
+              aria-sort="none"
+              class="text-start sortable"
+            >
+              <span>ชื่อเล่น</span
+              ><i
+                aria-hidden="true"
+                class="v-icon notranslate v-data-table-header__icon mdi mdi-arrow-up theme--light"
+                style="font-size: 18px"
+              ></i>
+            </th>
+            <th
+              role="columnheader"
+              scope="col"
+              aria-label="ระดับ: Not sorted. Activate to sort ascending."
+              aria-sort="none"
+              class="text-start sortable"
+            >
+              <span>ระดับ</span
+              ><i
+                aria-hidden="true"
+                class="v-icon notranslate v-data-table-header__icon mdi mdi-arrow-up theme--light"
+                style="font-size: 18px"
+              ></i>
+            </th>
+            <th
+              role="columnheader"
+              scope="col"
+              aria-label="ผู้สร้าง: Not sorted. Activate to sort ascending."
+              aria-sort="none"
+              class="text-start sortable"
+            >
+              <span>ผู้สร้าง</span
+              ><i
+                aria-hidden="true"
+                class="v-icon notranslate v-data-table-header__icon mdi mdi-arrow-up theme--light"
+                style="font-size: 18px"
+              ></i>
+            </th>
+            <th
+              role="columnheader"
+              scope="col"
+              aria-label="แจ้งชำระเงิน: Not sorted. Activate to sort ascending."
+              aria-sort="none"
+              class="text-start sortable"
+            >
+              <span>แจ้งชำระเงิน</span
+              ><i
+                aria-hidden="true"
+                class="v-icon notranslate v-data-table-header__icon mdi mdi-arrow-up theme--light"
+                style="font-size: 18px"
+              ></i>
+            </th>
+            <th
+              role="columnheader"
+              scope="col"
+              aria-label="สถานะ: Not sorted. Activate to sort ascending."
+              aria-sort="none"
+              class="text-start sortable"
+            >
+              <span>เปิดใช้งาน</span
+              ><i
+                aria-hidden="true"
+                class="v-icon notranslate v-data-table-header__icon mdi mdi-arrow-up theme--light"
+                style="font-size: 18px"
+              ></i>
+            </th>
+            <th
+              role="columnheader"
+              scope="col"
+              aria-label="Actions"
+              class="text-start"
+            >
+              <span>Actions</span>
+            </th>
+          </tr>
+        </thead>
+        <tbody class="bg-purple-100 cursor-pointer">
+          <tr class="" v-for="member in members" :key="member._id">
+            <td class="text-start">{{ member.username }}</td>
+            <td class="text-start">{{ member.name }}</td>
+            <td class="text-start">{{ member.role }}</td>
+            <td class="text-start">{{ member.createdBy.name }}</td>
+            <td class="text-start">
+              <label class="switch">
+                <input
+                  type="checkbox"
+                  :checked="member.cans.includes(confirmPaymentKey)"
+                  v-on:change="onChangeConfirmPayment(member)"
+                />
+                <div class="slider round">
+                  <span class="on">ได้</span>
+                  <span class="off">ไม่ได้</span>
+                </div>
+              </label>
+            </td>
+            <td class="text-start">
+              <label class="switch">
+                <input
+                  type="checkbox"
+                  :checked="member.isActive"
+                  v-on:change="onChangeActive(member)"
+                />
+                <div class="slider round">
+                  <span class="on">ปกติ</span>
+                  <span class="off">แบน</span>
+                </div>
+              </label>
+            </td>
+            <td class="text-start">
+              <nuxt-link :to="`/members/${member._id}`">
+                <button
+                  class="bg-gray-500 flex items-center px-3 py rounded hover:bg-green-700"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="24px"
+                    viewBox="0 0 24 24"
+                    width="24px"
+                    fill="#FFFFFF"
+                  >
+                    <path d="M0 0h24v24H0V0z" fill="none" />
+                    <path
+                      d="M19.43 12.98c.04-.32.07-.64.07-.98 0-.34-.03-.66-.07-.98l2.11-1.65c.19-.15.24-.42.12-.64l-2-3.46c-.09-.16-.26-.25-.44-.25-.06 0-.12.01-.17.03l-2.49 1c-.52-.4-1.08-.73-1.69-.98l-.38-2.65C14.46 2.18 14.25 2 14 2h-4c-.25 0-.46.18-.49.42l-.38 2.65c-.61.25-1.17.59-1.69.98l-2.49-1c-.06-.02-.12-.03-.18-.03-.17 0-.34.09-.43.25l-2 3.46c-.13.22-.07.49.12.64l2.11 1.65c-.04.32-.07.65-.07.98 0 .33.03.66.07.98l-2.11 1.65c-.19.15-.24.42-.12.64l2 3.46c.09.16.26.25.44.25.06 0 .12-.01.17-.03l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.03.24.24.42.49.42h4c.25 0 .46-.18.49-.42l.38-2.65c.61-.25 1.17-.59 1.69-.98l2.49 1c.06.02.12.03.18.03.17 0 .34-.09.43-.25l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.65zm-1.98-1.71c.04.31.05.52.05.73 0 .21-.02.43-.05.73l-.14 1.13.89.7 1.08.84-.7 1.21-1.27-.51-1.04-.42-.9.68c-.43.32-.84.56-1.25.73l-1.06.43-.16 1.13-.2 1.35h-1.4l-.19-1.35-.16-1.13-1.06-.43c-.43-.18-.83-.41-1.23-.71l-.91-.7-1.06.43-1.27.51-.7-1.21 1.08-.84.89-.7-.14-1.13c-.03-.31-.05-.54-.05-.74s.02-.43.05-.73l.14-1.13-.89-.7-1.08-.84.7-1.21 1.27.51 1.04.42.9-.68c.43-.32.84-.56 1.25-.73l1.06-.43.16-1.13.2-1.35h1.39l.19 1.35.16 1.13 1.06.43c.43.18.83.41 1.23.71l.91.7 1.06-.43 1.27-.51.7 1.21-1.07.85-.89.7.14 1.13zM12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 6c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z"
+                    />
+                  </svg>
+                  <span class="m-2 text-white">ตั้งค่า </span>
+                </button>
+              </nuxt-link>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </section>
 </template>
 
@@ -203,90 +230,4 @@ export default {
 </script>
 
 <style>
-.modal {
-  z-index: 1000;
-  transition: opacity 0.25s ease;
-}
-.modal-active {
-  overflow-x: hidden;
-  overflow-y: visible !important;
-}
-.switch {
-  position: relative;
-  display: inline-block;
-  width: 90px;
-  height: 34px;
-}
-
-.switch input {
-  display: none;
-}
-
-.slider {
-  position: absolute;
-  cursor: pointer;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: #ca2222;
-  -webkit-transition: 0.4s;
-  transition: 0.4s;
-}
-
-.slider:before {
-  position: absolute;
-  content: '';
-  height: 26px;
-  width: 26px;
-  left: 4px;
-  bottom: 4px;
-  background-color: white;
-  -webkit-transition: 0.4s;
-  transition: 0.4s;
-}
-
-input:checked + .slider {
-  background-color: #2ab934;
-}
-
-input:focus + .slider {
-  box-shadow: 0 0 1px #2196f3;
-}
-
-input:checked + .slider:before {
-  -webkit-transform: translateX(55px);
-  -ms-transform: translateX(55px);
-  transform: translateX(55px);
-}
-
-.on {
-  display: none;
-}
-
-.on,
-.off {
-  color: white;
-  position: absolute;
-  transform: translate(-50%, -50%);
-  top: 50%;
-  left: 50%;
-  font-size: 10px;
-}
-
-input:checked + .slider .on {
-  display: block;
-}
-
-input:checked + .slider .off {
-  display: none;
-}
-
-.slider.round {
-  border-radius: 34px;
-}
-
-.slider.round:before {
-  border-radius: 50%;
-}
 </style>
